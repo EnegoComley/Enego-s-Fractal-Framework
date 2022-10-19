823f43c8302addb38f680192ffedf7daab59f698
EnegoComley <enegocomley@gmail.com>
2022-10-19 12:44:43 +0100

    Created the project
    Also added first class

---
+++ FractalFrameworkWPF/FractalFrameworkWPF/Classes/RgbaArray.cs
@@ -0,0 +1,22 @@
+﻿using FractalFrameworkWPF;
+namespace FractalFrameworkWPF.Classes;
+
+public class RgbaArray
+{
+    int[,][] myArray;
+    int myLength;
+    int myHeight;
+    
+    public RgbaArray(int newLength, int newHeight) {
+        myLength = newLength;
+        myHeight = newHeight;
+        myArray = new int[myLength,myHeight][];
+        for (int i = 0; i < myLength; i++)
+        {
+            for (int j = 0; j < myHeight; j++)
+            {
+                myArray[i, j] = new int[]{0,0,0};
+            }
+        }
+    }
+}
---
+++ FractalFrameworkWPF/FractalFrameworkWPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using FractalFrameworkWPF;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FractalFrameworkWPF.Classes;
+
+namespace FractalFrameworkWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RgbaArray my_array = new RgbaArray(100, 100);
+        }
+    }
+}
